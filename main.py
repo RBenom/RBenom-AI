f00ad4b2bb3d7bda99843e6e69025ac1bb431d82
RBenom <sevenupoff@gmail.com>
2026-07-07 15:24:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = "8980705868:AAH9N5gg2anmoOhWE6Xk4nwVe1n6jBw0VbM"
 
@@ -9,10 +9,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
-app = Application.builder().token(TOKEN).build()
+def main():
+    app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    app.run_polling()
 
 if __name__ == "__main__":
-    app.run_polling()
+    main()
