bb8bff0cfbccba544ac60bdd3ca98e2b4edd8f4d
RBenom <sevenupoff@gmail.com>
2026-07-07 12:17:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ app = Application.builder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
